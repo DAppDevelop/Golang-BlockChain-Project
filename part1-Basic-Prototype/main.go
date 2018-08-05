0d165ff1ec96e4cad5dc34cba9cd971fe6c684c8
chenysh521 <chenysh521@126.com>
2018-08-05 20:20:08 +0800

    no message

--- part1-Basic-Prototype/main.go
+++ part1-Basic-Prototype/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/boltdb/bolt"
+	"log"
 	"blockchain/part1-Basic-Prototype/BLC"
 	"fmt"
 )
 
 func main() {
+
+	//-------------------1.创建区块链、添加区块到区块链中
 	//blockchain := BLC.CreatBlockchainWithGenesisBlock()
 	//
 	//blockchain.AddBlockToBlockchain(
@@ -25,25 +29,76 @@ func main() {
 	//
 	//fmt.Println(blockchain)
 
-
-
-	//判断工作量证明是否有效
+	//--------------------2.判断工作量证明是否有效
 	//block := BLC.NewBlock("hehhehe", 0, []byte{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,})
 	//fmt.Println(block)
 	//
 	//proofOfWork := BLC.NewProofOfWork(block)
 	//fmt.Println(proofOfWork.IsValid())
 
-	//序列化
-	block := BLC.NewBlock("hehhehe", 0, []byte{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,})
+	//--------------------3.创建区块并将其序列化并保存到数据库中
+	//block := BLC.NewBlock(
+	//	"hehhehe",
+	//	0,
+	//	[]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,})
+	//
+	//fmt.Println(block)
 
-	fmt.Println(block)
+	//打开数据库
+	db, err := bolt.Open("block.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	bytes := block.Serialize()
+	defer db.Close()
 
-	fmt.Println(bytes)
+	//创建表
+	//err = db.Update(func(tx *bolt.Tx) error {
+	//	//直接获取表，如果不存在，创建
+	//	b := tx.Bucket([]byte("blockchain"))
+	//
+	//	if b == nil {
+	//		b, err = tx.CreateBucket([]byte("blockchain"))
+	//		if err != nil {
+	//			log.Panic("block table create failed...")
+	//		}
+	//	}
+	//
+	//	err := b.Put([]byte("l"), block.Serialize())
+	//
+	//	if err != nil {
+	//		log.Panic(err)
+	//	}
+	//
+	//	fmt.Println("写入成功！")
+	//	return nil
+	//})
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	block = BLC.DeserializeBlock(bytes)
+	//读取表里面的block
 
-	fmt.Println(block)
+	err = db.View(func(tx *bolt.Tx) error {
+
+		b := tx.Bucket([]byte("blockchain"))
+
+		if b == nil {
+			log.Panic("读取不到blockchain")
+		}
+
+		blockData := b.Get([]byte("l"))
+
+		block := BLC.DeserializeBlock(blockData)
+
+		fmt.Println("READBLOCK ", block)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
 }
