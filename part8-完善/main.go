7d98f127ece3f4f5c1fb832b3cca3acc091fbd11
Yancey_Chan <chenyuanshan@outlook1120.com>
2018-08-23 15:48:14 +0800

    尝试添加 for死循环

--- part8-完善/main.go
+++ part8-完善/main.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"go-BlockChain/part7-Network/BLC"
+	"os"
+	"fmt"
+	"bufio"
+	"log"
 )
 
 func main() {
 	////创建命令行对象
-	cli := BLC.CLI{}
-	cli.Run()
+	//for {
+		go signalListen()
+		cli := BLC.CLI{}
+		cli.Run()
+	//}
+
+}
+
+func signalListen() {
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please enter some input: ")
+	stringC := make(chan string)
+
+	go func(stringC chan string) {
+		for {
+			i := <- stringC
+			switch i {
+			case "Philip\n", "Ivo\n", "Chris\n":
+				fmt.Printf("Welcome %s\n", i)
+			default:
+				fmt.Println("You are not welcome here! Goodbye!")
+			}
+		}
+	}(stringC)
+
+	for  {
+		input, err := inputReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		stringC <- input
+
+	}
+
 }
